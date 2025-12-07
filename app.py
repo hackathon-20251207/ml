8a939f2ecb0ad125eb1eff7628c58ff64cc1b58d
Ivan <set-night@pm.me>
2025-12-07 16:58:28 +0300

    chore: port to 8085

--- app.py
+++ app.py
@@ -205,6 +205,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", "8080")),
+        port=int(os.getenv("PORT", "8085")),
         reload=os.getenv("RELOAD", "false").lower() == "true",
     )
